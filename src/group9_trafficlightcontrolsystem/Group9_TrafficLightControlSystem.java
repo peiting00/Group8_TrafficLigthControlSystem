7963dd55fe13a9cb0ef94c1bf6941d960e26e42a
Peiting Chew <i19017904@student.newinti.edu.my>
2021-09-29 19:08:29 +0800

    commit

---
+++ src/group9_trafficlightcontrolsystem/Group9_TrafficLightControlSystem.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package group9_trafficlightcontrolsystem;
+
+/**
+ *
+ * @author user
+ */
+public class Group9_TrafficLightControlSystem {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
